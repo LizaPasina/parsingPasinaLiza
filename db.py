96837c8c835608694a13c3c1956b2469f5cbcf1b
LizaPasina <104529963+LizaPasina@users.noreply.github.com>
2022-05-01 12:31:23 +0300

    Update db.py

--- db.py
+++ db.py
@@ -8,13 +8,13 @@ class Database:
         self.con = mysql.connector.connect(host='gendalf.cf', port=3308, user='root', password='1234567890', database='Pasina')
         self.cur = self.con.cursor()
 
-    def upload_phone(self, phone: Phone):
-        self.cur.execute('INSERT INTO phone (name, price, url) VALUES (%s,%s,%s)', (phone.name, phone.price, phone.url))
+    def upload_phones(self, phones: Phone):
+        self.cur.execute('INSERT INTO phones (name, price, url) VALUES (%s,%s,%s)', (phones.name, phones.price, phones.url))
 
     def commit(self):
         self.con.commit()
 
     def truncate(self):
-        self.cur.execute('TRUNCATE phone')
+        self.cur.execute('TRUNCATE phones')
 
 
